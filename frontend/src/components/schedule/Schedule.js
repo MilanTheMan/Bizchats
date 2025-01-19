975ef0a808f19923cb6170c0021fee82f9f860e6
AlexisGorospe <AlexisGorospe@github.com>
2025-01-18 20:38:30 -0500

    gonna do this later

--- frontend/src/components/schedule/Schedule.js
+++ frontend/src/components/schedule/Schedule.js
@@ -11,162 +11,165 @@ const Schedule = () => {
         <div id={"schedule"}>
             <h1>Schedule</h1>
 
-            <div id={"calendar"}>
-                <div id={"calendar_day_names"}>
-                    <p>Sunday</p>
-                    <p>Monday</p>
-                    <p>Tuesday</p>
-                    <p>Wednesday</p>
-                    <p>Thursday</p>
-                    <p>Friday</p>
-                    <p>Saturday</p>
+            <div id={"schedule_content"}>
+                <div id={"calendar"}>
+                    <div id={"calendar_day_names"}>
+                        <p>Sunday</p>
+                        <p>Monday</p>
+                        <p>Tuesday</p>
+                        <p>Wednesday</p>
+                        <p>Thursday</p>
+                        <p>Friday</p>
+                        <p>Saturday</p>
 
+                    </div>
+                    <div id={"calendar_days"}>{/*this will be a 7x5 grid, also every day is a placeholder*/}
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>29</p>
+                            <p></p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>30</p>
+                            <p>Assignment 1</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>31</p>
+                            <p>C23 Math Class Lab</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>1</p>
+                            <p>Today</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>2</p>
+                            <p>C23 Math Class Lecture</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>3</p>
+                            <p></p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>4</p>
+                            <p></p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 2</p>
+                        </div>
+                        <div className={"calendar_day"}>
+                            <p className={"calendar_day_number"}>5</p>
+                            <p>Start Assignment 3</p>
+                        </div>
+                    </div>
                 </div>
-                <div id={"calendar_days"}>{/*this will be a 7x5 grid, also every day is a placeholder*/}
 
-
-
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>29</p>
-                        <p></p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>30</p>
-                        <p>Assignment 1</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>31</p>
-                        <p>C23 Math Class Lab</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>1</p>
-                        <p>Today</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>2</p>
-                        <p>C23 Math Class Lecture</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>3</p>
-                        <p></p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>4</p>
-                        <p></p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                    <div className={"calendar_day"}>
-                        <p className={"calendar_day_number"}>5</p>
-                        <p>Start Assignment 2</p>
-                    </div>
-                </div>
+                <aside id={"schedule_event_list"}>
+                    sdajfklsdjfklasdjfkl;asdfjfl
+                </aside>
             </div>
         </div>
     )
