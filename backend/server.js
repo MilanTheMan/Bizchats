94572961032bd66f69bce07db6767c0ee1c735a2
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2025-01-23 11:52:30 -0500

    idk what to do

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+/*
+const emp_routes = require("./routes/emp_routes.js");
+const user_routes = require("./routes/user_routes.js");
+*/
+
