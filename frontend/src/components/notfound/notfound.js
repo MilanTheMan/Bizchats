750b497ef79431a0b705d557c5e6a31f901ca53f
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2025-01-22 19:14:49 -0500

    all i did here is add the header

--- frontend/src/components/notfound/notfound.js
+++ frontend/src/components/notfound/notfound.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import {Link} from "react-router-dom";
+import Header from "../header/Header";
 
 function NotFound(){
     return(
         <div id={"not_found"}>
+            <Header/>
             <h1>Whoops!</h1>
             <p>page not found</p>
         </div>
