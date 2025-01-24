66b207c410d97468680d46eee2ec9598c2bb5793
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2025-01-23 20:40:48 -0500

    fixed the css of the professional page

--- backend/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-
-/*
-const emp_routes = require("./routes/emp_routes.js");
-const user_routes = require("./routes/user_routes.js");
-*/
-
