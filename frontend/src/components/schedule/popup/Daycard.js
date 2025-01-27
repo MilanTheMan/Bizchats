4db3ee84e7eea5ac010102b8cbb3d05bbdc50744
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2025-01-27 11:37:56 -0500

    making the date cards lol

--- frontend/src/components/schedule/popup/Daycard.js
+++ frontend/src/components/schedule/popup/Daycard.js
@@ -6,6 +6,7 @@ const Daycard = () => {
     // if there's no value it won't appear
     const [titleSpaceColor, setTitleSpaceColor] = useState("#5715d1");
     const [title, setTitle] = useState("No Title Set")
+
     const [dateHappening, setDateHappening] = useState(); // date
     const [timeHappening, setTimeHappening] = useState(); // time
     const [instructor, setInstructor] = useState();
@@ -13,16 +14,30 @@ const Daycard = () => {
 
     const [description, setDescription] = useState();
 
-    
-    
-    
+    const html = (
+        <div>
+
+
+
+        </div>
+    )
+
+    function detailConstructor(){
+
+    }
+
+
+    detailConstructor();
+
     
     return (
         <div className={"day_card"}>
             <div className={"title_container"}>
                 <p>{title}</p>
             </div>
-            <div className={"actual_content"}></div>
+            <div className={"actual_content"}>
+                {dateHappening ? (<p>Happening on {dateHappening}</p>) : ''} {/*do the same for the rest*/}
+            </div>
         </div>
     )   
 }
