76d3839c659d0eeaad6e6b52f88d88a226e2e30b
Milan Man <MilanTheMan23@gmail.com>
2025-01-29 17:30:05 -0500

    fixed some things

--- database.sql
+++ database.sql
@@ -49,8 +49,43 @@
 		profile_picture BLOB,
 		foreign key (role_id) references roles(id)
 	);
+    
+    
+	-- ---------- --
+	-- |Channels| --
+	-- ---------- --
 
 
+	drop table if exists channels;
+	create table channels
+	(
+		id INT PRIMARY KEY auto_increment,
+		name VARCHAR(100),
+		role_id INT NOT NULL,
+		profile_picture BLOB,
+		foreign key (role_id) references roles(id)
+	);
+    
+    
+    -- ------------------- --
+	-- |Users to Channels| --
+	-- ------------------- --
+
+
+	drop table if exists userstochannels;
+	create table userstochannels
+	(
+		userid INT,
+		channelid INT,
+		name VARCHAR(100),
+		picture BLOB,
+		creation_date DATETIME default NOW(),
+		foreign key (userid) references users(id) ON DELETE CASCADE,
+		foreign key (channelid) references channels(id) ON DELETE CASCADE,
+        PRIMARY KEY(userid, channelid)
+	);
+    
+    
 	-- -------------------- --
 	-- -------------------- --
 	-- |Insert Statements| --
@@ -69,9 +104,9 @@
     
     
 	INSERT INTO users (name, email, password, role_id, profile_picture) VALUES
-	("dummy1", "dummyemail1@fakemail.com", "password", 1, "123"),
-	("dummy2", "dummyemail2@fakemail.com", "password", 1, "123"),
-	("dummy3", "dummyemail3@fakemail.com", "password", 1, "123"),
-	("dummy4", "dummyemail4@fakemail.com", "password", 1, "123"),
-	("dummy5", "dummyemail5@fakemail.com", "password", 1, "123"),
-	("dummy6", "dummyemail6@fakemail.com", "password", 1, "123");
+	("dummy1", "dummyemail1@fakemail.com", "password", 1, NULL),
+	("dummy2", "dummyemail2@fakemail.com", "password", 1, NULL),
+	("dummy3", "dummyemail3@fakemail.com", "password", 1, NULL),
+	("dummy4", "dummyemail4@fakemail.com", "password", 1, NULL),
+	("dummy5", "dummyemail5@fakemail.com", "password", 1, NULL),
+	("dummy6", "dummyemail6@fakemail.com", "password", 1, NULL);
