3de5a3b0feff3b08c779e37d9d492c3c304b3cac
AlexisGorospe <156434534+AlexisGorospe@users.noreply.github.com>
2025-03-26 15:20:06 -0400

    changed the not found page to look better

--- frontend/src/components/notfound/notfound.js
+++ frontend/src/components/notfound/notfound.js
@@ -5,8 +5,10 @@ import Header from "../header/Header";
 function NotFound(){
     return(
         <div id={"not_found"}>
-            <h1>Whoops!</h1>
-            <p>page not found</p>
+            <div>
+                <h1>Whoops!</h1>
+                <p>page not found</p>
+            </div>
         </div>
     )
 }
